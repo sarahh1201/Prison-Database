41504a32a843793ea447acd0451226feacfee264
hmsa7 <164420546+hmsa7@users.noreply.github.com>
2024-03-25 20:18:25 -0700

    Update InmateDatabaseFunctions.h

--- InmateDatabaseFunctions.h
+++ InmateDatabaseFunctions.h
@@ -1,9 +1,9 @@
-
 #ifndef INMATEDATABASEFUNCTIONS_H
 #define INMATEDATABASEFUNCTIONS_H
 #include "Inmate.h"
 #include <string>
 #include <fstream>
+#include "UserDatabaseFunctions.h"
 
 using namespace std;
 
@@ -106,6 +106,48 @@ void setInmateInfo(bool canSetInmateInfo, int inmateIndex, int fieldIndex, strin
 	//catch() ACCESS DENIED: MISSING EDIT PERMISSIONS
 }
 
-//Inmate login();
+Inmate logInfo(string username, string password)
+{
+		ifstream input("Inmate_Usernames.txt");//open the usernames and password file (order of info is: index, username, password)
+		//look for the username
+		int i = 0;
+		string inUsername;
+		int staffIndex;
+		bool match = false;
+		while (getline(input, inUsername, ' '))
+		{
+
+			string inPassword;
+			if ((i % maxUsernameFileIndex) / 1 == 1)//skip every entry that is not usernames 
+				if (inUsername == username)//if the usernames match
+				{
+					input >> inPassword;//take in the next entry into passwords
+					i++;
+					//try{ make the below a compare function that throws the excpetion
+					if (inPassword == password)//if the passwords match
+					{
+						int index = i / maxUsernameFileIndex;
+						input.close();
+						string userData[maxIndices];
+						string inmateData[inmateMaxIndices];
+
+
+						for (int j = 0; j < maxIndices; j++)
+							userData[j] = getUserInfo(true, 0, index, j);
+
+
+						for (int j = 0; j < inmateMaxIndices; j++)
+							inmateData[j] = getInmateInfo(true, index, j);
+
+						return Inmate(true, index, userData, inmateData);
+					}
+					//}
+					//catch() PASSWORDS DONT MATCH
+				}
+			i++;
+		}
+		input.close();
+	
+}
 #endif
 
