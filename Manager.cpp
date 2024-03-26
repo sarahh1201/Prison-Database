3995906141e6d98479e8d116a907ac3f6f8e9254
hmsa7 <164420546+hmsa7@users.noreply.github.com>
2024-03-25 20:17:12 -0700

    Update Manager.cpp

--- Manager.cpp
+++ Manager.cpp
@@ -1,1 +1,18 @@
 #include "Manager.h"
+
+
+
+Manager::Manager(bool canCreateStaff, int staffIndex, string userData[], string managerData[])
+{
+	//manager fields (same as a staff object)
+	position = managerData[1];
+	scheduleGroup = managerData[2];
+	//user fields
+	this->userIndex = userIndex;
+	firstname = userData[1];
+	lastname = userData[2];
+	username = "username";//default settings, if changed then username will be updated
+	password = "password";//default setting, if changed then username will be updated
+	govID = userData[3];
+	userID = userData[4];
+}
