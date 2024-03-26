dff71ef946695f5f1a7d439ffa93c7a7ebf201a0
hmsa7 <164420546+hmsa7@users.noreply.github.com>
2024-03-25 19:06:27 -0700

    Update Staff.cpp

--- Staff.cpp
+++ Staff.cpp
@@ -4,3 +4,26 @@ Staff::Staff()
 {
 
 }
+Staff::Staff(bool canCreateStaff, int userIndex, string userData[], string staffData[])
+{
+	//staff fields
+	position = staffData[1];
+	scheduleGroup = staffData[2];
+	//user fields
+	this->userIndex = userIndex;
+	firstname = userData[1];
+	lastname = userData[2];
+	username = "username";//default settings, if changed then username will be updated
+	password = "password";//default setting, if changed then username will be updated
+	govID = userData[3];
+	userID = userData[4];
+}
+
+string Staff::getPosition()
+{
+	return position;
+}
+string Staff::getScheduleGroup()
+{
+	return scheduleGroup;
+}
