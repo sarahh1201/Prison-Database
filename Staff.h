2bcbfb6b37822f560c251cf784364d7d71d50c8a
hmsa7 <164420546+hmsa7@users.noreply.github.com>
2024-03-25 19:06:49 -0700

    Update Staff.h

--- Staff.h
+++ Staff.h
@@ -5,18 +5,36 @@
 
 using namespace std;
 
+
+const int staffMaxIndices = 3;//max number of fields in staff data file
 class Staff : public User
 {
 protected:
 	string position;
-	char scheduleGroup;
+	string scheduleGroup;
 
 public:
 	Staff();
 	//Staff(bool canCreateStaff);
 	//Staff(all user data fields);
-	//Staff(all user data fields, all staff data fields);
-	~Staff();
-	void deleteStaff(bool canCreateStaff);
+	Staff(bool canCreateStaff, int staffIndex, string userData[], string staffData[]);
+	//~Staff();
+	//void deleteStaff(bool canCreateStaff);
+	string getPosition();
+	string getScheduleGroup();
+
+	void operator=(Staff newStaff)
+	{
+		position = newStaff.getPosition();
+		scheduleGroup = newStaff.getScheduleGroup();
+		firstname = newStaff.getFirstname();
+		lastname = newStaff.getLastname();
+		username = newStaff.getUsername();
+		password = newStaff.getPassword();
+		govID = newStaff.getGovID();
+		userID = newStaff.getUserID();
+		userIndex = newStaff.getUserIndex();
+	}
 };
 #endif
+
