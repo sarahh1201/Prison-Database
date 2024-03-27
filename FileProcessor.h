06cea480b6c60efc9aeb5fb69f67d41d3153b286
JennyHalbert <jennykatehalbert@gmail.com>
2024-03-26 21:20:41 -0700

    FileProcessor.h

---
+++ FileProcessor.h
@@ -0,0 +1,15 @@
+//
+// Created by Jenny Halbert on 2024-03-26.
+//
+
+#ifndef WELCOMESCREEN_FILEPROCESSOR_H
+#define WELCOMESCREEN_FILEPROCESSOR_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "EntryException.h"
+
+void processFile(const std::string& filename);
+
+#endif //WELCOMESCREEN_FILEPROCESSOR_H
