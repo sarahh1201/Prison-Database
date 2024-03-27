4604649455fb5d25e49721fcd545a39ca8635e22
hmsa7 <164420546+hmsa7@users.noreply.github.com>
2024-03-26 21:28:05 -0700

    Add files via upload
    version 4, no manager files

---
+++ V4_Staff.h
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef V4_STAFF_H
+#define V4_STAFF_H
+#include <string>
+#include <sstream>
+#include "V4_User.h"
+
+using namespace std;
+
+
+class Staff : public User
+{
+public:
+	string position;
+	string scheduleGroup;
+
+	Staff();
+	Staff(bool canCreateStaff, int staffIndex, string userData[], string staffData[]);
+	~Staff() {}
+	string getPosition();
+	string getScheduleGroup();
+	void deleteAccount(string username, int accountType);
+	void createAccount(int accountType);
+
+	void operator=(Staff newStaff)
+	{
+		position = newStaff.getPosition();
+		scheduleGroup = newStaff.getScheduleGroup();
+		firstname = newStaff.getFirstname();
+		lastname = newStaff.getLastname();
+		username = newStaff.getUsername();
+		password = newStaff.getPassword();
+		govID = newStaff.getGovID();
+		userID = newStaff.getUserID();
+		userIndex = newStaff.getUserIndex();
+	}
+};
+#endif
+
