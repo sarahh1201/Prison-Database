7e6a62d9e964e7d20ecc7411d715e42680144578
hmsa7 <164420546+hmsa7@users.noreply.github.com>
2024-03-29 14:40:59 -0700

    Update V4_Test.cpp

--- V4/V4_Test.cpp
+++ V4/V4_Test.cpp
@@ -295,25 +295,26 @@ void staffView(string& user, Staff& object) {
         cout << "Block " << (char)blocks[0][i] << ": " << blocks[1][i] << endl;
     }
 
-    //Manager selection menu
+    //--------------------------------------------------------------------Staff Selection Menu 1--------------------------------------------------------------------------------------
     cout << "\nSelect an option: " << endl;
     cout << "1. Prisoner info"
         << "\n2. View Schedule"
-        << "\n3. Edit User Info"
-        << "\n4. Logout"
+        << "\n3. View User Info"
+        << "\n4. Edit User Info"
+        << "\n5. Logout"
         << endl;
 
     cout << "Option: ";
     int option;
     cin >> option;
 
-    while (option != 1 && option != 2 && option != 3 && option != 4) {
+    while (option <1 ||option>5) {
         cout << "Invalid input." << endl;
         cin >> option;
     }
 
     switch (option) {
-    case 1: //Prisoner info
+    case 1: //----------------------------Prisoner info
     {
         clearScreen();
         //Call prisoner function
@@ -336,7 +337,7 @@ void staffView(string& user, Staff& object) {
         }
         break;
     }
-    case 2: //View schedule
+    case 2: //----------------------------View Schedule
     {
         clearScreen();
         string filename = "StaffSchedule.txt";
@@ -363,14 +364,43 @@ void staffView(string& user, Staff& object) {
 
         break;
     }
-    case 3: //Edit Info
+    case 3://-----------------------------View User Info
+    {
+        cout << "Account Information"
+            << "\nUsername: "<< user
+            << "\nFirst Name: " << object.getFirstname()
+            << "\nLast Name: " << object.getLastname()
+            << "\nGovernment ID: " << object.getGovID()
+            << "\nUser ID: " << object.getUserID()
+            << "\nPosition: " << object.getPosition()
+            << "\nSchedule Group: " << object.getScheduleGroup()
+            << "\nEnter 1 to return" << endl;
+            int exit;
+            cin >> exit;
+
+            while (exit != 1)
+            {
+                clearScreen();
+                cout << "Invalid input!" << endl;
+                cout << "Enter 1 to go back: ";
+                cin >> exit;
+            }
+
+            if (exit == 1)
+            {
+                clearScreen();
+                staffView(user, object);
+            }
+        break;
+    }
+    case 4: //----------------------------Edit Info
     {
         clearScreen();
 
         cout << "Select an option: "
-            << "\n1. "
-            << "\n2. "
-            << "\n3. "
+            << "\n1. Edit Personal Data"
+            << "\n2. Edit Inmate Data"
+            << "\n3. Edit Staff Data"
             << "\n4. Return" << endl;
 
         cout << "Option: ";
@@ -380,24 +410,348 @@ void staffView(string& user, Staff& object) {
             cout << "Invalid input. Please enter a valid option. " << endl;
             cin >> option;
         }
-        switch (option)
+        switch (option)//Choose Account type to edit
         {
-        case 1: {
+        case 1: //Edit personal data
+        {
+            char ans = 'x';
+            do {
+                cout << "Select a field to edit: "
+                    << "\n1. Username"
+                    << "\n2. First Name: " << object.getFirstname()
+                    << "\n3. Last Name: " << object.getLastname()
+                    << "\n4. Government ID: " << object.getGovID()
+                    << "\n5. User ID: " << object.getUserID()
+                    << "\n6. Position: " << object.getPosition()
+                    << "\n7. Schedule Group: " << object.getScheduleGroup()
+                    << "\n8. Return" << endl;
 
-        }
-        case 2: {
+                cout << "Option: ";
+                cin >> option;
 
-        }
-        case 3: {
+                while (option > 8 || option < 1)
+                {
+                    cout << "Invalid input. Please enter a valid option. " << endl;
+                    cin >> option;
+                }
 
+                string data;
+                switch (option)//Choose Edit Field
+                {
+                case 1://Username
+                {
+                    cout << "Please enter your new username: ";
+                    cin >> data;
+                    if (object.newUsername(0, data))
+                        object.setUsername(data);
+                    else
+                        cout << "Username " << data << " is already in use.";
+                    staffView(user, object);
+                    break;
+                }
+                case 2://First name
+                {
+                    cout << "Please enter your first name: ";
+                    cin >> data;
+                    object.setFirstname(data);
+                    staffView(user, object);
+                    break;
+                }
+                case 3://Last name
+                {
+                    cout << "Please enter your last name: ";
+                    cin >> data;
+                    object.setLastname(data);
+                    staffView(user, object);
+                    break;
+                }
+                case 4://Government ID
+                {
+                    cout << "Please enter new Government ID Number: ";
+                    cin >> data;
+                    object.setGovID(data);
+                    staffView(user, object);
+                    break;
+                }
+                case 5://User ID
+                {
+                    cout << "Please enter new User ID: ";
+                    cin >> data;
+                    object.setUserID(data);
+                    staffView(user, object);
+                    break;
+                }
+                case 6://Position
+                {
+                    cout << "Please enter new Position: ";
+                    cin >> data;
+                    object.setPosition(data);
+                    staffView(user, object);
+                    break;
+                }
+                case 7://Schedule Group
+                {
+                    cout << "Please enter new Schedule Group: ";
+                    cin >> data;
+                    object.setScheduleGroup(data);
+                    staffView(user, object);
+                    break;
+                }
+                case 8://-----------------------------------Return-------------------------------CURRENT TESTING POINT-------------------
+                {        cout << "\nWould you like to save any changes? (y/n)" << endl;
+                cin >> ans;
+                while (ans != 'y' && ans != 'n' && ans != 'Y' && ans != 'N')
+                {
+                    cout << "\nInvalid Input: Please enter again." << endl;
+                    cin >> ans;
+                }
+                if (ans == 'y' || ans == 'Y')
+                {object.save();}
+                    clearScreen();
+                    staffView(user, object);
+                    break;
+                }
+                }
+                break;
+            } while (ans != 'y' || ans != 'Y' || ans!='n' || ans!='N');
         }
-        case 4:{
+        case 2: //Edit inmate data
+        {
+            clearScreen();
+            cout << "Enter username of account: ";
+            string data;
+            cin >> data;
+           
+            //Edit inmate data 
+            if (!object.newUsername(1, data))
+            {
+                int inmateIndex = object.staffGetUserIndex(1, data);
+                cout << "Select an option: "
+                    << "\n1. Username: " << object.staffGetUsername(1, inmateIndex)
+                    << "\n2. First Name:" << object.getUserInfo(1, inmateIndex, 1)
+                    << "\n3. Last Name:" << object.getUserInfo(1, inmateIndex, 2)
+                    << "\n4. Government ID: " << object.getUserInfo(1, inmateIndex, 3)
+                    << "\n5. User ID:" << object.getUserInfo(1, inmateIndex, 4)
+                    << "\n6. Lawyer Name:" <<object.getInmateInfo(inmateIndex, 1)
+                    << "\n7. Probation Date:" << object.getInmateInfo(inmateIndex, 2)
+                    << "\n8. Incarceration Date:" << object.getInmateInfo(inmateIndex, 3)
+                    << "\n9. Cell Number:" << object.getInmateInfo(inmateIndex, 4)
+                    << "\n10. Cell Block:" << object.getInmateInfo(inmateIndex, 5)
+                    << "\n11. Roommate ID:" << object.getInmateInfo(inmateIndex, 6)
+                    << "\n12. Schedule Group:" << object.getInmateInfo(inmateIndex, 7)
+                    << "\n13. Return" << endl;
+
+                cout << "Option: ";
+                cin >> option;
+
+                while (option > 13 || option < 1)
+                {
+                    cout << "Invalid input. Please enter a valid option. " << endl;
+                    cin >> option;
+                }
+
+                //Choice of inmate Edits
+                switch (option)
+                {
+                case 1://Username
+                {
+                    cout << "Please enter the new username: ";
+                    cin >> data;
+                    object.setUsernameInfo(1, inmateIndex, 1, data);
+                    break;
+                }
+                case 2://First name
+                {
+                    cout << "Please enter first name: ";
+                    cin >> data;
+                    object.setUserInfo(1, inmateIndex, 1, data);
+                    break;
+                }
+                case 3://Last name
+                {
+                    cout << "Please enter last name: ";
+                    cin >> data;
+                    object.setUserInfo(1, inmateIndex, 2, data);
+                    break;
+                }
+                case 4://Government ID
+                {
+                    cout << "Please enter new Government ID Number: ";
+                    cin >> data;
+                    object.setUserInfo(1, inmateIndex, 3, data);
+                    break;
+                }
+                case 5://User ID
+                {
+                    cout << "Please enter new User ID: ";
+                    cin >> data;
+                    object.setUserInfo(1, inmateIndex, 4, data);
+                    break;
+                }
+                case 6://Lawyer last name
+                {
+                    cout << "Please enter new lawyer last name: ";
+                    cin >> data;
+                    object.setInmateInfo(inmateIndex, 1, data);
+                    break;
+                }
+                case 7://Probation Date
+                {
+                    cout << "Please enter new probation date: ";
+                    cin >> data;
+                    object.setInmateInfo(inmateIndex, 2, data);
+                    break;
+                }
+                case 8://Incarceration Date
+                {
+                    cout << "Please enter new incarceration date: ";
+                    cin >> data;
+                    object.setInmateInfo(inmateIndex, 3, data);
+                    break;
+                }
+                case 9://Cell Number
+                {
+                    cout << "Please enter new cell number: ";
+                    cin >> data;
+                    object.setInmateInfo(inmateIndex, 4, data);
+                    break;
+                }
+                case 10://Cell Block
+                {
+                    cout << "Please enter new cell block: ";
+                    cin >> data;
+                    object.setInmateInfo(inmateIndex, 5, data);
+                    break;
+                }
+                case 11://Roommate ID
+                {
+                    cout << "Please enter new Roommate ID: ";
+                    cin >> data;
+                    object.setInmateInfo(inmateIndex, 6, data);
+                    break;
+                }
+                case 12://Schedule Group
+                {
+                    cout << "Please enter new schedule group: ";
+                    cin >> data;
+                    object.setInmateInfo(inmateIndex, 7, data);
+                    break;
+                }
+                case 13://Return
+                {
+                    clearScreen();
+                    staffView(user, object);
+                    break;
+                }
+                }
+            }
+            else
+                cout << "\nUsername does not exist. " << endl;
+            break;
+        }
+        case 3: //Edit staff data
+        {
+            clearScreen();
+            cout << "Enter username of account: ";
+            string data;
+            cin >> data;
+
+            //Edit staff data 
+            if (!object.newUsername(0, data))
+            {
+                int staffInd = object.staffGetUserIndex(0, data);
+                cout << "Select an option: "
+                    << "\n1. Username: " << object.staffGetUsername(0, staffInd)
+                    << "\n2. First Name:" << object.getUserInfo(0, staffInd, 1)
+                    << "\n3. Last Name:" << object.getUserInfo(0, staffInd, 2)
+                    << "\n4. Government ID: " << object.getUserInfo(0, staffInd, 3)
+                    << "\n5. User ID:" << object.getUserInfo(0, staffInd, 4)
+                    << "\n6. Position:" << object.getStaffInfo(staffInd,1)
+                    << "\n7. Schedule Group:" << object.getStaffInfo(staffInd, 2)
+                    << "\n8. Return" << endl;
+
+                cout << "Option: ";
+                cin >> option;
+
+                while (option > 8 || option < 1)
+                {
+                    cout << "Invalid input. Please enter a valid option. " << endl;
+                    cin >> option;
+                }
+
+                //Choice of staff Edits
+                switch (option)
+                {
+                case 1://Username
+                {
+                    cout << "Please enter the new username: ";
+                    cin >> data;
+                    object.setUsernameInfo(0, staffInd, 1, data);
+                    break;
+                }
+                case 2://First name
+                {
+                    cout << "Please enter first name: ";
+                    cin >> data;
+                    object.setUserInfo(0, staffInd, 1, data);
+                    break;
+                }
+                case 3://Last name
+                {
+                    cout << "Please enter last name: ";
+                    cin >> data;
+                    object.setUserInfo(0, staffInd, 2, data);
+                    break;
+                }
+                case 4://Government ID
+                {
+                    cout << "Please enter new Government ID Number: ";
+                    cin >> data;
+                    object.setUserInfo(0, staffInd, 3, data);
+                    break;
+                }
+                case 5://User ID
+                {
+                    cout << "Please enter new User ID: ";
+                    cin >> data;
+                    object.setUserInfo(0, staffInd, 4, data);
+                    break;
+                }
+                case 6://Position
+                {
+                    cout << "Please enter new position: ";
+                    cin >> data;
+                    object.setInmateInfo(staffInd, 1, data);
+                    break;
+                }
+                case 7://Schedule Group
+                {
+                    cout << "Please enter new schedule group: ";
+                    cin >> data;
+                    object.setInmateInfo(staffInd, 2, data);
+                    break;
+                }
+                case 8://Return
+                {
+                    clearScreen();
+                    staffView(user, object);
+                    break;
+                }
+                }
+            }
+            else
+                cout << "\nUsername does not exist. " << endl;
+            break;
+        }
+        case 4: //Return to staff view
+        {
             clearScreen();
             staffView(user, object);
+            break;
         }
         }
     }
-    case 4: //Logout
+    case 5: //Logout
     {
         clearScreen();
         object.~Staff();
@@ -588,9 +942,6 @@ void managerView(string& user) {
     }
 }
 
-
-
-
 int main()
 {
     //testing functions
@@ -618,7 +969,7 @@ int main()
 		cout << "Username: " << staff1.getUsername() << endl;
 		cout << "Password: " << staff1.getPassword() << endl;
 
-
+        staff1.~Staff();
 
 //actual program
         bool inProgram = true;
@@ -661,25 +1012,24 @@ int main()
                 cin >> name;
                 cout << "Password: ";
                 cin >> password;
-                if (view == 1)
            
                 //Call login function
                 if (view == 1)
                 {
                     Inmate user;
-                    user.inmateLogin(name, password);
+                    user = user.inmateLogin(name, password);
                     prisonerView(name, user);
                 }
                 else if (view == 2)
                 {
                     Staff user;
-                    user.staffLogin(name, password);
+                    user = user.staffLogin(name, password);
                     staffView(name, user);
                 }
                 else if (view == 3)
                 {
                     Staff user;
-                    user.staffLogin(name, password);
+                    user = user.staffLogin(name, password);
                     managerView(name);
                 }
 
