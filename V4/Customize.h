dce605e4126911118900636e865422665cd4930b
Sarah <shy_h@telus.net>
2024-03-31 15:56:51 -0700

    Update Customize.h

--- V4/Customize.h
+++ V4/Customize.h
@@ -16,22 +16,20 @@ void clearScreen() {
     #endif
 }
 
-//colourCode = 34 for blue 
-
 //Change the colour of the text using the ANSI colour escape codes
 void colourize(const string& text, int colourCode) {
     cout << "\033[" << colourCode << "m" << text << "\033[0m";
 }
 
 void midBorders(const string& text) {
-    int width = 24; // Border width includes 4 spaces on each side of the text
+    int width = 24; // Border width 
 
     // Print text with borders
     cout << "| " << text << setw(width-text.length()) << setfill(' ') << left << right << '|' << endl;
 }
 
 void topBorders(const string& text) {
-    int width = 24; // Border width includes 4 spaces on each side of the text
+    int width = 24; // Border width 
 
     // Print top border
     cout << '+' << string(width, '-') << '+' << endl;
@@ -41,7 +39,7 @@ void topBorders(const string& text) {
 }
 
 void bottomBorders(const string& text) {
-    int width = 24; // Border width includes 4 spaces on each side of the text
+    int width = 24; 
 
     // Print text with borders
     cout << "| " << text << setw(width-text.length()) << setfill(' ') << left << right << '|' << endl;
@@ -51,7 +49,7 @@ void bottomBorders(const string& text) {
 }
 
 void allBorders(const string& text) {
-    int width = 24; // Border width includes 4 spaces on each side of the text
+    int width = 24; // Border width 
 
     // Print top border
     cout << '+' << string(width, '-') << '+' << endl;
@@ -106,5 +104,4 @@ void displayBlocks() {
         cerr << "Failed to open file: " << filename << endl;
 }
 }
-
 #endif
